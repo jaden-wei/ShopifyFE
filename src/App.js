978912442ee77b1c0117d9cc1aa796f76b7fb178
Jaden <jaden.wei2003@gmail.com>
2022-01-16 12:43:54 -0500

    pictures fetched and displayed

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
+  const [photos, setPhotos] = useState([]);
+
+  useEffect(() => {
+    getData();
+
+    async function getData() {
+      const response = await fetch(
+        "https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&api_key=DEMO_KEY"
+      );
+      const data = await response.json();
+
+      // store the data into our books variable
+      setPhotos(data.photos);
+    }
+  }, []);
+
+  console.log(photos);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Shopify Front End Developer Internship Project</h1>
+      {photos && (
+        <div className="photos">
+          {photos.map((photo, index) => (
+            <div key={index}>
+              <img src={photo.img_src} alt="Photo" />
+            </div>
+          ))}
+        </div>
+      )}
     </div>
   );
 }
