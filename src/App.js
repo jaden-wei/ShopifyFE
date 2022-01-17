5052ef13a688978def5af5c1ffc606abac0b60b2
jaden-wei <jaden.wei2003@gmail.com>
2022-01-17 12:50:56 -0500

    backdrop and search design

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ function App() {
 
     async function getData() {
       const response = await fetch(
-        "https://api.nasa.gov/planetary/apod?api_key=zFb9JRrtQmpRKW169NqZcsntRXYwNjrkcZDis3KH&count=25"
+        "https://api.nasa.gov/planetary/apod?api_key=zFb9JRrtQmpRKW169NqZcsntRXYwNjrkcZDis3KH&count=100"
       );
       const data = await response.json();
 
@@ -30,6 +30,16 @@ function App() {
 
   return (
     <div className="App">
+      <svg id="svg" xmlns="http://www.w3.org/2000/svg" viewBox="-300 0 950 270">
+        <path
+          d="M-314,267 C105,364 400,100 812,279"
+          fill="none"
+          stroke="white"
+          strokeWidth="120"
+          strokeLinecap="round"
+        />
+      </svg>
+
       <Header />
 
       {photos ? (
--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,12 +1,18 @@
 import React from "react";
 import "./styles.css";
+import { AiOutlineSearch } from "react-icons/ai";
 
 function Header() {
   return (
     <div className="header">
       <div></div>
       <h1 className="title">Spacetagram</h1>
-      <h3 className="subtitle">Developed by Jaden Wei</h3>
+      <h4 className="subtitle">Developed by Jaden Wei</h4>
+      <p>using NASA's Astronomy Photo of the Day API </p>
+      <div className="search-container">
+        <AiOutlineSearch className="search-icon" />
+        <input placeholder="Search (YYYY-MM-DD)" />
+      </div>
     </div>
   );
 }
